a506364aefb7f43b79eca9b6f6c49bb88ef1e8cd
dl-proj <vivi.shimoe.1986@gmail.com>
2020-11-23 07:09:14 -0300

    PDF processing

--- src/aws/manager.py
+++ src/aws/manager.py
@@ -1,12 +1,9 @@
 import boto3
 import configparser
 import os
-import numpy as np
-import cv2
 
-from pdf2image import convert_from_path
 from src.invoice_no.extractor import InvoiceExtractor
-from settings import PDF_IMAGES_DIR, CONFIG_FILE
+from settings import CONFIG_FILE
 
 
 class AWSManager:
@@ -24,14 +21,9 @@ class AWSManager:
 
         for obj in object_listing['Contents']:
             path, filename = os.path.split(obj["Key"])
-            if filename != "":
-                file_path = os.path.join(PDF_IMAGES_DIR, filename)
-                frame_path = os.path.join(PDF_IMAGES_DIR, f"{filename.replace('.pdf', '')}.jpg")
-                print(f"[INFO] {filename} downloading...")
-                self.s3.download_file(self.source_bucket, obj["Key"], file_path)
-                pdf_images = [np.array(page) for page in convert_from_path(file_path, 200)]
-                cv2.imwrite(frame_path, cv2.cvtColor(pdf_images[0], cv2.COLOR_BGR2RGB))
-                invoice_no = self.invoice_extractor.extract_invoice_no(image_path=frame_path)
+            if "pdf" in filename:
+                invoice_no = self.invoice_extractor.extract_invoice_no(pdf_name=filename,
+                                                                       s3_bucket_name=self.source_bucket)
                 copy_source = {'Bucket': self.source_bucket, 'Key': filename}
                 self.s3.copy(copy_source, self.new_bucket, f"{invoice_no}.pdf")
                 self.s3.delete_object(Bucket=self.source_bucket, Key=filename)
--- src/invoice_no/extractor.py
+++ src/invoice_no/extractor.py
@@ -1,5 +1,6 @@
 import boto3
 import configparser
+import time
 
 from settings import CONFIG_FILE
 
@@ -11,24 +12,89 @@ class InvoiceExtractor:
         self.textract = boto3.client('textract', region_name=params.get("DEFAULT", "region_name"),
                                      aws_access_key_id=params.get("DEFAULT", "access_key_id"),
                                      aws_secret_access_key=params.get("DEFAULT", "secret_access_key"))
+        self.job_id = None
 
-    def extract_invoice_no(self, image_path):
+    def start_job(self, s3_bucket_name, object_name):
+        response = self.textract.start_document_text_detection(
+            DocumentLocation={'S3Object': {'Bucket': s3_bucket_name, 'Name': object_name}})
+
+        return response["JobId"]
+
+    def is_job_complete(self):
+        time.sleep(5)
+        response = self.textract.get_document_text_detection(JobId=self.job_id)
+        status = response["JobStatus"]
+        print(f"[INFO] Job status: {status}")
+
+        while status == "IN_PROGRESS":
+            time.sleep(5)
+            response = self.textract.get_document_text_detection(JobId=self.job_id)
+            status = response["JobStatus"]
+            print(f"[INFO] Job status: {status}")
+
+        return status
+
+    def get_job_results(self):
+
+        pages = []
+
+        time.sleep(5)
+
+        response = self.textract.get_document_text_detection(JobId=self.job_id)
+
+        pages.append(response)
+        print(f"[INFO] Resultset page recieved: {len(pages)}")
+        next_token = None
+        if 'NextToken' in response:
+            next_token = response['NextToken']
+
+        while next_token:
+            time.sleep(5)
+
+            response = self.textract.get_document_text_detection(JobId=self.job_id, NextToken=next_token)
+
+            pages.append(response)
+            print(f"[INFO] Resultset page recieved: {len(pages)}")
+            next_token = None
+            if 'NextToken' in response:
+                next_token = response['NextToken']
+
+        return pages
+
+    def extract_invoice_no(self, pdf_name, s3_bucket_name):
         invoice_no = ""
-        with open(image_path, 'rb') as document:
-            image_bytes = bytearray(document.read())
+        response = []
+        bounding_left = 0
+        bounding_right = 0
+        bounding_top = 0
+        bounding_bottom = 0
 
-        # Call Amazon Textract
-        response = self.textract.detect_document_text(Document={'Bytes': image_bytes})
-        invoice_info = response["Blocks"][1:]
+        self.job_id = self.start_job(s3_bucket_name=s3_bucket_name, object_name=pdf_name)
+        print(f"[INFO] Started job with id: {self.job_id}")
+        if self.is_job_complete():
+            response = self.get_job_results()
+        invoice_info = response[0]["Blocks"][1:]
 
         # Print detected text
         for i, item in enumerate(invoice_info):
             if "Invoice" in item["Text"]:
                 invoice_no = invoice_info[i + 1]["Text"]
+                bounding_left = item["Geometry"]["BoundingBox"]["Left"] + item["Geometry"]["BoundingBox"]["Width"]
+                bounding_right = item["Geometry"]["BoundingBox"]["Left"] + 3 * item["Geometry"]["BoundingBox"]["Width"]
+                bounding_top = item["Geometry"]["BoundingBox"]["Top"] - 0.002
+                bounding_bottom = \
+                    item["Geometry"]["BoundingBox"]["Top"] + item["Geometry"]["BoundingBox"]["Height"] + 0.002
+                break
+
+        for item in invoice_info:
+            item_left = item["Geometry"]["BoundingBox"]["Left"]
+            item_top = item["Geometry"]["BoundingBox"]["Top"]
+            if bounding_left <= item_left <= bounding_right and bounding_top <= item_top <= bounding_bottom:
+                invoice_no = item["Text"]
                 break
 
         return invoice_no
 
 
 if __name__ == '__main__':
-    InvoiceExtractor().extract_invoice_no(image_path="")
+    InvoiceExtractor().extract_invoice_no(pdf_name="", s3_bucket_name="")
